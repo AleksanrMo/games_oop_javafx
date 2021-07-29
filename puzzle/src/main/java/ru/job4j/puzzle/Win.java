296ac6eb75edc26c9bf9cd98f9ef2cde191e9f5a
AleksandrMo <sushanta@mail.ru>
2021-07-29 17:51:59 +0700

    сокобан

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,7 +2,60 @@ package ru.job4j.puzzle;
 
 public class Win {
     public static boolean check(int[][] board) {
-        boolean rsl = true;
+        boolean rsl = false;
+        if (vertical(board) || horizontal(board)) {
+                rsl = true;
+        }
+
         return rsl;
+    }
+
+    public static boolean horizontal(int[][] board) {
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == 1) {
+                    sum += 1;
+                }
+            }
+            if(sum == 5) {
+                result = true;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    public static boolean vertical(int[][] board) {
+        boolean result = false;
+        int sum = 0;
+        int point = 0;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = point; j < board[i].length; j++) {
+                if (board[i][j] == 1) {
+                    sum += 1;
+                    point = j;
+                    break;
+            }
+            }
+            if(sum == 5) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[][] board = {
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0},
+        };
+        System.out.println(check(board));
     }
 }
