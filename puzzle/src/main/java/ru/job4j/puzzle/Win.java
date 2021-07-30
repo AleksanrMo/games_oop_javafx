0a52dad705b3290dfb1d8b066712df7b1af1cf2f
AleksandrMo <sushanta@mail.ru>
2021-07-30 16:10:54 +0700

    сокобан

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -3,45 +3,28 @@ package ru.job4j.puzzle;
 public class Win {
     public static boolean check(int[][] board) {
         boolean rsl = false;
-        if (vertical(board) || horizontal(board)) {
+        if (monoHorizontal(board, 2) || monoVertical( board, 2)) {
             rsl = true;
         }
 
         return rsl;
     }
 
-    public static boolean horizontal(int[][] board) {
-        boolean result = false;
-        for (int i = 0; i < board.length; i++) {
-            int sum = 0;
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == 1) {
-                    sum += 1;
-                }
-            }
-            if (sum == 5) {
-                result = true;
+    public static boolean monoHorizontal(int[][] board, int row) {
+        boolean result = true;
+        for (int j = 0; j < board.length; j++) {
+            if (board[row][j] != 1) {
+                result = false;
                 break;
             }
         }
-
         return result;
     }
-
-    public static boolean vertical(int[][] board) {
-        boolean result = false;
-        int sum = 0;
-        int point = 0;
+    public static boolean monoVertical(int[][] board, int column) {
+        boolean result = true;
         for (int i = 0; i < board.length; i++) {
-            for (int j = point; j < board[i].length; j++) {
-                if (board[i][j] == 1) {
-                    sum += 1;
-                    point = j;
-                    break;
-                }
-            }
-            if (sum == 5) {
-                result = true;
+            if (board[i][column] != 1) {
+                result = false;
                 break;
             }
         }
