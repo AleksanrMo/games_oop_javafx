b81d3945a40f78f0de084e334a3ff901f4aa1bfc
AleksandrMo <sushanta@mail.ru>
2021-09-05 17:21:11 +0700

    checkstyle

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -8,14 +8,16 @@ import ru.job4j.chess.firuges.black.BishopBlack;
 public class LogicTest {
 
     @Test(expected = ImpossibleMoveException.class)
-    public void whenImpossibleMove() throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+    public void whenImpossibleMove() throws FigureNotFoundException,
+            OccupiedCellException, ImpossibleMoveException {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.A1));
         logic.move(Cell.A1, Cell.B1);
     }
 
     @Test(expected = OccupiedCellException.class)
-    public void whenOccupiedCell() throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+    public void whenOccupiedCell() throws FigureNotFoundException,
+            OccupiedCellException, ImpossibleMoveException {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.A1));
         logic.add(new BishopBlack(Cell.B2));
@@ -23,7 +25,8 @@ public class LogicTest {
     }
 
     @Test(expected = FigureNotFoundException.class)
-    public void whenFigureNotFound() throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+    public void whenFigureNotFound() throws FigureNotFoundException,
+            OccupiedCellException, ImpossibleMoveException {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.A1));
         logic.move(Cell.B1, Cell.C2);
